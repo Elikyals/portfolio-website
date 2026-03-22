b4d57beb7898c7e7a5152219eb95e4fea54da523
elikyals <kofiyalley16@gmail.com>
2026-03-22 18:20:26 -0400

    fix: add base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://eliyahukyalley.dev',
+    base: '/portfolio-website',
     output: 'static'
 });
