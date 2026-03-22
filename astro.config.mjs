ac62dfcd88146a34612c2c2342b3a81c21033b54
elikyals <kofiyalley16@gmail.com>
2026-03-22 19:45:38 -0400

    fix: add CNAME for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://eliyahukyalley.dev',
-    base: '/portfolio-website',
     output: 'static'
 });
